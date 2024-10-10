f0e80ccb270fbe32274592713c94d7b232059c0e
s1v <thank_area@yahoo.co.jp>
2024-10-11 00:46:00 +0900

    プロジェクト ファイルを追加します。

---
+++ NocoBaseAPITester/Models/NocoBaseData.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NocoBaseAPITester.Models
+{
+    [JsonConverter(typeof(NocoBaseDataJsonConverter))]
+    public class NocoBaseData : List<Dictionary<string, JsonElement>>
+    {
+        public HashSet<string> Keys
+        {
+            get
+            {
+                if (Count == 0)
+                {
+                    return new HashSet<string>();
+                }
+
+                var commonKeys = new HashSet<string>(this[0].Keys);
+                for (int i = 1; i < Count; i++)
+                {
+                    commonKeys.IntersectWith(this[i].Keys);
+                }
+
+                return commonKeys;
+            }
+        }
+    }
+
+    public class NocoBaseDataJsonConverter : JsonConverter<NocoBaseData>
+    {
+        public override NocoBaseData Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException("Expected start of array.");
+            }
+
+            var data = new NocoBaseData();
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return data;
+                }
+
+                var item = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(ref reader, options);
+                data.Add(item);
+            }
+
+            throw new JsonException("Expected end of array.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, NocoBaseData value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, (List<Dictionary<string, JsonElement>>)value, options);
+        }
+    }
+}
---
+++ NocoBaseAPITester/Models/NocoBaseResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NocoBaseAPITester.Models
+{
+    public class NocoBaseResponse
+    {
+        [JsonPropertyName("data")]
+        public NocoBaseData Data { get; set; }
+        [JsonPropertyName("meta")]
+        public MetaData Meta {  get; set; }
+    }
+
+    public class MetaData
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+
+        [JsonPropertyName("pageSize")]
+        public int PageSize { get; set; }
+
+        [JsonPropertyName("totalPage")]
+        public int TotalPage { get; set; }
+    }
+}
